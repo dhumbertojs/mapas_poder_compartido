2304d0d4276981b9d4fd4be57039965b478d54f3
David Humberto Jiménez Sánchez <dhumbertojs@gmail.com>
2019-05-02 00:23:05 -0500

    Corrección de escaños en la Cámara de Diputados. Primer mapa de cambio municipal (San Luis Potosí)

--- congreso_camara_baja.R
+++ congreso_camara_baja.R
@@ -18,7 +18,8 @@ party <- c("Sin_Grupo" = "#ff3399", "MC" = "#ff8000", "Morena" = "#b30000",
            "PAN" = "#005ce6", "PRD" = "#ffff00", "PRI" = "#ff0000", 
            "PVEM" = "#00cc44","NPP" = "#8C0F6D", "PCP" = "#DC551E",    
            "PDM" = "#CE0536", "PES" = "#8701F7", "PNA" = "#01F7CA",
-           "PT" = "#DD0FB4", "Conv" = "#ff8000", "PAS" = "#800000", "PSD" = "#ff6666")
+           "PT" = "#DD0FB4", "Conv" = "#ff8000", "PAS" = "#800000", 
+           "PSD" = "#ff6666", "PSN" = "#a64dff", "INDEP" = "#666699")
 
 ##a esto le hace falta un for##
 x <- 1
@@ -29,7 +30,7 @@ pb = txtProgressBar(min=1, max=length(legis), style=3)
 for (x in 1:length(legis)) {
   tempo <- dip %>% filter(year == legis[x])
   dipus <- parliament_data(tempo, 8, tempo$Total, type = "semicircle", tempo$GP)
-  plot <- ggplot(dipus, aes(x, y, colour = GP, size = 3L)) +
+  plot <- ggplot(dipus, aes(x, y, colour = GP, size = 2L)) +
     geom_parliament_seats() + 
     theme_ggparliament() +
     scale_color_manual(values = party) +
@@ -43,5 +44,5 @@ for (x in 1:length(legis)) {
 list.files(path = out, pattern = "*.png", full.names = T) %>% 
   map(image_read) %>% 
   image_join() %>% 
-  image_animate(fps = 1) %>% 
+  image_animate(fps = 0.5) %>% 
   image_write(paste(out,"camara_baja.gif", sep = "/"))
---
+++ mapa_slp.R
@@ -0,0 +1,84 @@
+rm(list=ls())
+setwd("~")
+
+####Directorios y paquetes####
+inp <- "/Users/Emmanuel RB/Documents/El Poder Compartido II"
+out <- "/Users/Emmanuel RB/Documents/El Poder Compartido II/Output/Ayuntamientos/SLP"
+maps <- "/Users/Emmanuel RB/Documents/Mapas (INEGI,shp)/conjunto de datos"
+
+library(rgdal)
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(purrr)
+library(magick)
+####Llamar y limpiar datos####
+slp <- read.csv(paste(inp, "EPC2 - Municipios (1980-2019).csv", sep = "/"), fileEncoding = "UTF-8", stringsAsFactors = F)
+slp <- slp %>% filter(Estado == "San_Luis_Potosi") %>% select(-c(4:23))
+
+elec <- 2000:2019
+san <- data.frame()
+x <- 1
+pb = txtProgressBar(min=1, max=length(elec), style=3)
+
+for (x in 1:length(elec)) {
+  tempo <- slp %>% select(INEGI, ends_with(as.character(elec[x]))) %>% mutate(year = elec[x])
+  names(tempo) <- c("INEGI", "win", "year")
+  san <- bind_rows(san, tempo)
+  setTxtProgressBar(pb, x)
+  rm(tempo)
+}
+
+san$win = sapply(strsplit(san$win, "_"), "[", 1)
+slp <- slp %>% select(Municipio, INEGI)
+
+for(x in 1:length(elec)) {
+  tempo = filter(san, year == elec[x]) %>% select(-year)
+  slp = full_join(slp, tempo, by = "INEGI")
+  rm(tempo)
+  setTxtProgressBar(pb, x)
+}
+
+elec <- 1998:2019
+for(x in 3:length(elec)) {
+  colnames(slp)[x] <- elec[x]
+}
+
+slp <- slp %>% mutate(
+  CVE_MUN = substr(INEGI, 3, 5)
+) %>% select(-c("Municipio", "INEGI"))
+####Abrir shp####
+map <- readOGR(paste(maps, "24mun.shp", sep = "/"))
+map@data$id <- rownames(map@data)
+mapa <- merge(map, slp, by.x = "CVE_MUN", by.y = "CVE_MUN")
+map_slp <- fortify(mapa, region = "id")
+fin <- merge(map_slp, mapa@data, by = "id")
+####Plotear####
+party <- c("INDEP" = "#ff3399", "MC" = "#ff8000", "Morena" = "#b30000",
+           "PAN" = "#005ce6", "PRD" = "#ffff00", "PRI" = "#ff0000", 
+           "PVEM" = "#00cc44","NPP" = "#8C0F6D", "PCP" = "#DC551E",    
+           "PDM" = "#CE0536", "PES" = "#8701F7",    "PNA" = "#01F7CA",
+           "PT" = "#DD0FB4")
+pb = txtProgressBar(min=1, max=length(fin), style=3)
+elec <- 1989:2019
+dada <- as.character(1989:2019)
+for (x in 12:length(fin)) {
+  try <- ggplot() + 
+    geom_polygon(data = fin, aes(long, lat, group = group, fill = unlist(fin[x])), color = "#FFFFFF") +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), 
+          axis.text = element_blank(), 
+          axis.ticks = element_blank()) +
+    scale_fill_manual(values = party, limit = levels(as.factor(unlist(fin[x]))), name = "Partido") +
+    labs(title = "Alcaldías de San Luis Potosí", subtitle = as.character(colnames(fin[x])), col = "Partido", x = "", y = "") +
+    coord_fixed(1.1)
+  ggsave(paste(dada[x], ".png", sep = ""), path = out, dpi = 300)
+  setTxtProgressBar(pb, x)
+  rm(try)
+}
+####Animacion/GIF####
+list.files(path = out, pattern = "*.png", full.names = T) %>% 
+  map(image_read) %>% #función de Purrr(map), las otras son funciones de magick
+  image_join() %>% 
+  image_animate(fps = 1) %>% 
+  image_write(paste(out,"SLP_2000-2019.gif", sep = "/"))
