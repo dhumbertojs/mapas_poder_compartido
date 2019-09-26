9b1aa21c0275a519d3587585c5b9e320eba62188
dhumbertojs <dhumbertojs@gmail.com>
2019-09-25 23:13:15 -0500

    Verificación de datos

---
+++ Verificación y limpieza de datos.R
@@ -0,0 +1,30 @@
+rm(list = ls())
+setwd("~")
+
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(janitor)
+library(stringr)
+
+out <- "/home/dhjs/Documentos/R_projects/mapas_poder_compartido"
+
+data <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQYNRmqrZVq5rctj6BEj_aM_f9oQRY_G-xnDMIgyjsiMnpsiy_9HHlbbpzFwj7pEV-u4Rgy3LaUw55P/pub?gid=266728315&single=true&output=csv", stringsAsFactors = F)
+
+tidy <- data %>% 
+  select(1:3, 24:43) %>% 
+  pivot_longer(
+    X2000:X2019, 
+    names_to = "Anio", 
+    values_to = "Ganador", 
+    names_prefix = "X")
+
+#Candelaria, Camp; no tuvo elecciones? 
+#Igual y siempre ganó el PRI
+
+try <- tidy %>% 
+  filter(Ganador == "")
+table(try$Estado)
+
+#filter(!str_detect(filtro, "Sección") & !str_detect(filtro, "Básica") 
+##Filtro para renglones con texto!!
